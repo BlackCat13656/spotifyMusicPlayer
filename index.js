23f168890500b8a822da2c4214e903a3eeacee70
Ekan <ekanessien@gmail.com>
2025-09-26 18:43:26 -0700

    Adding JavaScript to make the player work

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const songData = [
+  {
+    name: "Color Out",
+    artist: "Alone",
+    src: "Color Out - Alone.mp3",
+  },
+  {
+    name: "Do I",
+    artist: "Leeona",
+    src: "Do I - Leeona.mp3",
+  },
+  {
+    name: "Radio Nowhere",
+    artist: "Molotov Heart",
+    src: "Radio Nowhere - Molotov Heart.mp3",
+  },
+];
 
+const container = document.querySelector(".container");
+const songName = document.querySelector(".song-name");
+const songArtist = document.querySelector(".song-artist");
+const cover = document.querySelector(".cover");
+const playPauseBtn = document.querySelector(".play-pause");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const audio = document.querySelector(".audio");
+const songTime = document.querySelector(".song-time");
+const songProgress = document.querySelector(".song-progress");
+const coverName = document.querySelector(".cover span:nth-child(2)");
+const coverArtist = document.querySelector(".cover span:nth-child(1)");
+let songIndex = 0;
